fececccb37c7379346f123ab4696b5f42ee8ed09
morin <alexeymorin@gmail.com>
2023-03-09 22:46:26 +0300

    Пример получения данных API openweathermap.org

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [temp, setTemp] = useState('');
+  const [desc, setDesc] = useState('');
+  const [icon, setIcon] = useState('');
+  const [isReady, setReady] = useState(false);
+  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${process.env.REACT_APP_CITY}&APPID=${process.env.REACT_APP_API_KEY}&units=metric`;
+
+  useEffect(() => {
+    fetch(URL)
+    .then(result => result.json())
+    .then(jsonresult => {
+      setTemp(jsonresult.main.temp);
+      setDesc(jsonresult.weather[0].main);
+      setIcon(jsonresult.weather[0].icon);
+      setReady(true);
+    })
+    .catch(err => console.error(err));
+  });
+
+  if (isReady) {
+    return (
+      <div className="App">
+        <p>Temperature: {temp}</p>
+        <p>Description: {desc}</p>
+        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="Weather icon"></img>
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>
+  };
+
 }
 
 export default App;
